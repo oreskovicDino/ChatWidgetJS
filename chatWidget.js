2d32eaffaec9d6eea8bdbf1d0ea4989db58bf5ca
Dino Orešković <oreskovic.dino@gmail.com>
2023-12-21 10:45:00 +0000

    js fn test

--- chatWidget.js
+++ chatWidget.js
@@ -1,1 +1,7 @@
 console.log("chatWidget");
+
+// "position: fixed; bottom: 0; right: 0; width: 52px; height: 52px; color: #fff; background-color: #0360a5; background-position: center center; background-repeat: no-repeat; box-shadow: 12px 15px 20px 0 rgba(46, 61, 73, 0.15); border: 0; border-radius: 50%; cursor: pointer;"
+
+(function () {
+    console.log("chatWidget from fn") ;
+})
